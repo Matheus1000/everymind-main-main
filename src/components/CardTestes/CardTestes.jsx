57cc4f67ad57fc87566a1a507c7a7c054a78974d
CaiqueL <lpscaca@gmail.com>
2023-09-27 00:19:26 -0300

    commit CardTestes não funcional

--- src/components/CardTestes/CardTestes.jsx
+++ src/components/CardTestes/CardTestes.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { Button } from "../Button";
 import { Link } from "react-router-dom";
-
+import Alert from "../AlertaCustom/AlertaCustom";
 import "./style.css";
 
-export const CardTestes = ({}) => {
+export const CardTestes = ({ name, role, status }) => {
   
+  const handleButtonClick = () => {
+    let alertMessage = "";
+
+    if (status === "Aguardando") {
+      alertMessage = "Este processo está aguardando aprovação";
+    } else if (status === "Em andamento") {
+      alertMessage = "Este processo está em andamento";
+    } else if (status === "Realizar teste") {
+      alertMessage = "Usuário deverá ser transferido para área de testes";
+    }
+
+    <Alert message={alertMessage} />
+    
+  
+  };
+
   return (
     <div className="modal">
       <div className="modal-content">
         <div className="user">
           <div className="stack2">
-            <div className="name-rand">Cielo</div>
-            <div className="role-rand">Junior</div>
+            <div className="name-rand">{name}</div>
+            <div className="role-rand">{role}</div>
           </div>
-          <Button 
+          <Button
             className="button-instance7"
             color="inherit"
             labelClassName="button-2"
@@ -23,13 +39,11 @@ export const CardTestes = ({}) => {
             text="Iniciar Teste"
             variant="contained"
             background="none"
+            onClick={handleButtonClick}
           />
-
           <div className="icon-button-wrapper"></div>
         </div>
-      </div>{" "}
+      </div>
     </div>
   );
 };
-
-export default CardTestes;
