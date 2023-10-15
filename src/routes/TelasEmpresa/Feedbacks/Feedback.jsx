16b8a15aa80de51e9e0f45f5f961e117c5e23e02
CaiqueL <lpscaca@gmail.com>
2023-10-14 22:04:21 -0300

    telas empresa

---
+++ src/routes/TelasEmpresa/Feedbacks/Feedback.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+const Feedback = () => {
+
+};
+
+export default Feedback;
---
+++ src/routes/TelasEmpresa/vagasCadastradas/vagasCadastradas.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+const VagasCadastradas = () => {
+
+};
+
+export default VagasCadastradas;
